582663e8b4cda270d0011ddbb51d769f0e60fd1e
ricardgo <ricardo.gonzalez@openmarket.com>
2021-04-09 18:14:00 -0500

    Fix: Delete recursive call inside toString

--- src/main/java/com/example/springcourse/models/Book.java
+++ src/main/java/com/example/springcourse/models/Book.java
@@ -81,7 +81,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 
